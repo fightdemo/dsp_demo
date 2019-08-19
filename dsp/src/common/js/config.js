65ef9472a947e4d17ec47d8b3e083d2be04769a5
likai <likai@hubcloud.com.cn>
2019-08-19 17:24:33 +0800

    delete

--- dsp/src/common/js/config.js
+++
@@ -1,5 +0,0 @@
-const baseURL = 'http://test.ads.hubcloud.com.cn';
-// const baseURL2 = 'http://test.dmp.hubcloud.com.cn';
-const baseURL2 = 'http://dmp.hubcloud.com.cn';
-const uploadUrl = 'http://res1.hubcloud.com.cn'
-export {baseURL,baseURL2,uploadUrl}
